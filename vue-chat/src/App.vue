f8e5148ebbf2d25f9efaa9551f1d7e6cbea59e70
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-11-23 12:45:29 +0200

    username and messages state rendered in app.

--- vue-chat/src/App.vue
+++ vue-chat/src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="container">
-    <div
-      class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white"
-      style="width: 380px"
-    >
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
       <a
         href="/"
         class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom"
       >
-        <svg class="bi pe-none me-2" width="30" height="24">
-          <use xlink:href="#bootstrap"></use>
-        </svg>
-        <span class="fs-5 fw-semibold">List group</span>
+        <input class="fs-5 fw-semibold" v-model="username" />
       </a>
       <div class="list-group list-group-flush border-bottom scrollarea">
-        <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
+        <a
+          class="list-group-item list-group-item-action py-3 lh-sm"
+          v-for="message in messages"
+          :key="message"
+        >
           <div class="d-flex w-100 align-items-center justify-content-between">
-            <strong class="mb-1">List group item heading</strong>
-            <small class="text-muted">Tues</small>
-          </div>
-          <div class="col-10 mb-1 small">
-            Some placeholder content in a paragraph below the heading and date.
-          </div>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
-          <div class="d-flex w-100 align-items-center justify-content-between">
-            <strong class="mb-1">List group item heading</strong>
-            <small class="text-muted">Tues</small>
+            <strong class="mb-1">{{ message.username }}</strong>
+            <small class="text-muted"></small>
           </div>
           <div class="col-10 mb-1 small">
             Some placeholder content in a paragraph below the heading and date.
@@ -38,8 +27,18 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "App",
+  setup() {
+    const username = ref("username");
+    const messages = ref([]);
+
+    return {
+      username,
+      messages,
+    };
+  },
 };
 </script>
 
