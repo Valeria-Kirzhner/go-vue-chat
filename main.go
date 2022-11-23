fc0d0906127f256c52541922d8c89a89c8f40ab3
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-11-23 11:38:01 +0200

    created fiber server.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+    app := fiber.New()
+	app.Use(cors.New())
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":8000"))
+}
