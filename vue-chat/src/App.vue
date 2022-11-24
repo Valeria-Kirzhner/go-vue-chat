b8f2e5b199d783308f78ce2fdc92992ecf265dfa
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-11-24 09:32:24 +0200

    bugs fixed, added message time, vue chat app done.

--- vue-chat/src/App.vue
+++ vue-chat/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
     <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-      <a
-        href="/"
+      <div
         class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom"
       >
         <input class="fs-5 fw-semibold" v-model="username" />
-      </a>
+      </div>
       <div class="list-group list-group-flush border-bottom scrollarea">
         <a
           class="list-group-item list-group-item-action py-3 lh-sm"
@@ -15,31 +14,82 @@
         >
           <div class="d-flex w-100 align-items-center justify-content-between">
             <strong class="mb-1">{{ message.username }}</strong>
-            <small class="text-muted"></small>
+            <small class="text-muted">{{ currentDate }}</small>
           </div>
-          <div class="col-10 mb-1 small">
-            Some placeholder content in a paragraph below the heading and date.
-          </div>
+          <div class="col-10 mb-1 small">{{ message.message }}</div>
         </a>
       </div>
     </div>
+    <form @submit.prevent="onSubmit">
+      <input
+        class="form-controll"
+        placeholder="Write a message"
+        v-model="message"
+      />
+    </form>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import Pusher from "pusher-js";
+
 export default {
   name: "App",
   setup() {
     const username = ref("username");
     const messages = ref([]);
+    const message = ref("");
+    const currentDate = ref("");
+
+    onMounted(() => {
+      Pusher.logToConsole = true;
+
+      const pusher = new Pusher("88553fa8f6ec1411c857", {
+        cluster: "ap2",
+      });
+
+      const channel = pusher.subscribe("chat");
+      channel.bind("message", (data) => {
+        getDate();
+        messages.value.push(data);
+      });
+    });
+
+    function getDate() {
+      const current = new Date();
+      const time = `${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
+      const dateAndTime = `${time}  ${current.getDate()}/${
+        current.getMonth() + 1
+      }/${current.getFullYear()}`;
+      return (currentDate.value = dateAndTime);
+    }
+
+    const onSubmit = async () => {
+      await fetch("http://localhost:8000/api/messages", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username.value,
+          message: message.value,
+        }),
+      });
+      message.value = "";
+    };
 
     return {
       username,
       messages,
+      message,
+      onSubmit,
+      currentDate,
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.scrollarea {
+  min-height: 500px;
+}
+</style>
